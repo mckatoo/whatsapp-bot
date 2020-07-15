f35eeea3a8e77166f6e4b7fa08fef8dbed4feab0
Milton Carlos Katoo <mckatoo@gmail.com>
2020-07-14 21:12:32 -0300

    feat: add google speech recognition

--- src/messages.ts
+++ src/messages.ts
@@ -2,14 +2,19 @@ import {
   getNotificationType,
   MessageType,
   WAClient,
+  decodeMediaMessage,
 } from "@adiwajshing/baileys";
 import { Principal, Pizzas, Lanches, Porcoes } from "./menus";
+import speech from "@google-cloud/speech";
+import fs from "fs";
+import { google } from "@google-cloud/speech/build/protos/protos";
 
 class Messages {
   async verifyMessages(client: WAClient) {
     client.setOnUnreadMessage(true, async (m) => {
       const [notificationType, messageType] = getNotificationType(m);
       console.log("Recebeu notificação do tipo: " + notificationType);
+      console.log("Recebeu mensagem do tipo: " + messageType);
 
       if (notificationType !== "message") {
         return;
@@ -29,7 +34,7 @@ class Messages {
         const text = m.message?.conversation;
         console.log(sender + " Enviou: " + text);
         if (text != "") {
-            Principal(sender, client, MessageType.text);
+          Principal(sender, client, MessageType.text);
         }
         switch (text) {
           case "menu":
@@ -47,6 +52,43 @@ class Messages {
           default:
             break;
         }
+      } else if (
+        messageType === MessageType.audio &&
+        sender != null &&
+        m.message != null
+      ) {
+        const audio = await decodeMediaMessage(
+          m.message,
+          `./public/audio/audio_in_${m.key.id}`
+        );
+        const audioFile = fs.readFileSync(audio.toString());
+
+        // INICIO DA TRANSCRIÇÃO DO AUDIO EM TEXTO
+        const googleSpeechClient = new speech.SpeechClient();
+        const audioBytes = audioFile;
+        const audioEncoded = {
+          content: audioBytes,
+        };
+        const configEncode: google.cloud.speech.v1.IRecognitionConfig = {
+          encoding: "LINEAR16",
+          sampleRateHertz: 8000,
+          languageCode: "pt-BR",
+        };
+        const request: google.cloud.speech.v1.IRecognizeRequest = {
+          audio: audioEncoded,
+          config: configEncode,
+        };
+        const [response] = await googleSpeechClient.recognize(request);
+        const transcription = response.results
+          ?.map((result: google.cloud.speech.v1.ISpeechRecognitionResult) => {
+            if (result.alternatives != null) {
+              result.alternatives[0].transcript;
+            }
+          })
+          .join("\n") || "audio não reconhecido";
+        console.log(`Transcrisção: ${transcription}`);
+        client.sendMessage(sender, audioFile, MessageType.audio);
+        client.sendMessage(sender, transcription, MessageType.text);
       }
     });
   }
